dffe4d5b8b9a3cc45fcbb813349ec7b47d60b703
Corey Crosby <ccc_13@live.ca>
2019-06-04 12:50:23 -0400

    completed

---
+++ exercise.py
@@ -0,0 +1,27 @@
+ballots = [{'gold': 'Smudge', 'silver': 'Tigger', 'bronze': 'Simba'},
+           {'gold': 'Bella', 'silver': 'Lucky', 'bronze': 'Tigger'},
+           {'gold': 'Bella', 'silver': 'Boots', 'bronze': 'Smudge'},
+           {'gold':'Boots', 'silver': 'Felix', 'bronze': 'Bella'},
+           {'gold': 'Lucky', 'silver': 'Felix', 'bronze': 'Bella'},
+           {'gold': 'Smudge', 'silver': 'Simba', 'bronze': 'Felix'}]
+
+votes = {
+    'Smudge': 0,
+    'Tigger': 0,
+    'Simba': 0,
+    'Bella': 0,
+    'Lucky': 0,
+    'Boots': 0,
+    'Felix': 0
+    }
+
+for ballot in ballots:
+    for key, value in ballot.items():
+        if key == 'gold':
+            votes[value] += 3
+        elif key == 'silver':
+            votes[value] += 2
+        elif key == 'bronze':
+            votes[value] += 1
+winner = max(votes, key=votes.get)
+print(winner)
